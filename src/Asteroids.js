dca7b5b778af8b8616adb2c21120437f589bc4c9
Ian Price <michael.ian.price@gmail.com>
2017-05-18 22:20:10 -0400

    basic concept complete

---
+++ src/Asteroids.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Asteroids extends React.Component {
+    render() {
+        return (
+            <div>
+                Background: Astronomy Picture of the Day (NASA)
+                <br/>
+                <br/>
+                <br/>
+                Foreground: Enter your date of birth to find out which asteroid you rode here on!
+            </div>
+        );
+    }
+};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Asteroids from './Asteroids';
+import './index.css';
+
+ReactDOM.render(
+  <Asteroids />,
+  document.getElementById('root')
+);
