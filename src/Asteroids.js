141856f30cf89427c85a0dd0818049ff38dc755f
Ian Price <michael.ian.price@gmail.com>
2017-05-19 18:03:34 -0400

    astronomy picture of the day api call setup

--- src/Asteroids.js
+++ src/Asteroids.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import $ from 'jquery';
 
 export default class Asteroids extends React.Component {
+    componentDidMount() {
+        this.props.apod_url = getAPOD();
+    }
     render() {
         return (
             <div>
-                Background: Astronomy Picture of the Day (NASA)
+                <img src={this.props.apod_url} alt="APOD" />
                 <br/>
                 <br/>
                 <br/>
@@ -13,3 +17,18 @@ export default class Asteroids extends React.Component {
         );
     }
 };
+
+function getAPOD() {
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.nasa.gov/planetary/apod?api_key=2I118L1MuSm4DnJz2cgcwA8aCe1YTe6zP1WWhFtO',
+        contentType: 'json'
+    })
+    .then((result) => {
+        console.log(`Date: ${result.title}`);
+        console.log(`Date: ${result.date}`);
+        console.log(`Date: ${result.explanation}`);
+        return result.url
+    })
+    .catch((error) => {throw error});
+}
